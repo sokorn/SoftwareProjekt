982fbcf51232a26e38d84860d0b413f41f4a1eb6
Qler77 <marco.rentschler@arcor.de>
2014-03-11 13:22:50 +0100

    Beans und LocalBeans für alle Database Entities angelegt. UserSessionBean ausprogrammiert

--- carrental/carrental-ejb/src/java/controller/UserSessionBean.java
+++ carrental/carrental-ejb/src/java/controller/UserSessionBean.java
@@ -1,10 +1,13 @@
 package controller;
 
 import java.util.Date;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.FlushModeType;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import model.Adress;
 import model.User;
 
 @Stateless(name = "SessionBean")
@@ -13,18 +16,51 @@ public class UserSessionBean implements UserSessionBeanLocal {
     @PersistenceContext
     private EntityManager entityManager;
 
+    //TODO ADRESS
     @Override
-    public User createUser(String name, Date birthdate, String loginname, String title, String firstname, String lastname) {
+    public User createUser(String mail, Date birthdate, String loginname, String title, String firstname, String lastname, Adress adress) {
       entityManager.setFlushMode(FlushModeType.AUTO);
-        User user = new User(name, birthdate, loginname, title, firstname, lastname);
+        
+        User user = new User(mail, birthdate, loginname, title, firstname, lastname);
         entityManager.persist(user);
         user = entityManager.merge(user);
         entityManager.flush();
         return user;
     }
-
+//TODO Passwortprüfung
     @Override
     public User login(String login, String passwort) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Query query = entityManager.createNamedQuery("User.findByMail");
+        query.setParameter("mail", login);
+        List queryResult = query.getResultList();
+        return (User) queryResult.get(0);
+    }
+
+    @Override
+    public void changeFirstname(User user, String newFirstname) {
+        user.setFirstname(newFirstname);
+        entityManager.merge(this);
+        entityManager.flush();
+    }
+
+    @Override
+    public void changeLastname(User user, String newLastname) {
+       user.setFirstname(newLastname);
+       entityManager.merge(this);
+       entityManager.flush();
+    }
+
+    @Override
+    public void changeMail(User user, String newMail) {
+       user.setFirstname(newMail);
+       entityManager.merge(this);
+       entityManager.flush();
+    }
+
+    @Override
+    public void changeTitle(User user, String newTitle) {
+        user.setFirstname(newTitle);
+        entityManager.merge(this);
+        entityManager.flush();
     }
 }
