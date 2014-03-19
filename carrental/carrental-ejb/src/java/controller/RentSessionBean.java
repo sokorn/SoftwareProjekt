e9cbe264a7dab55d0eba6e1472d99aee21b11386
Qler77 <marco.rentschler@arcor.de>
2014-03-19 10:04:13 +0100

    lauffähig gemacht xD

--- carrental/carrental-ejb/src/java/controller/RentSessionBean.java
+++ carrental/carrental-ejb/src/java/controller/RentSessionBean.java
@@ -1,8 +1,37 @@
 package controller;
 
+import java.util.Date;
 import javax.ejb.Stateless;
+import model.Car;
+import model.Rent;
+import model.User;
 
 @Stateless(name = "RentsSessionBean")
 public class RentSessionBean implements RentSessionBeanLocal {
+
+    @Override
+    public Double getRentPrice(Car car) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getLengthOfRent(Date startDate, Date endDate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void blockCar(Car car) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Rent createRent(User user, Car car, double totalPrice, int length, Date startDate, Date endDate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void cancelRent(Rent rent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
